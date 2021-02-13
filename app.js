667b8d3ec789d68c00e0d753272704308e6a968e
Kukuh Alfian <kukuh@majoo.id>
2021-02-13 21:59:02 +0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const app = express();
+
+// Router
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const coursesRouter = require('./routes/courses');
+const mediaRouter = require('./routes/media');
+const paymentsRouter = require('./routes/payments');
+const ordersRouter = require('./routes/orders');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/courses', coursesRouter);
+app.use('/media', mediaRouter);
+app.use('/payments', paymentsRouter);
+app.use('/orders', ordersRouter);
+
+module.exports = app;
